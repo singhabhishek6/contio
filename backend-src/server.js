36e5e4dc5c96e8c9fd88a2a47c5b8607961aeb4c
Kumar Saurav <krsaurav1007@gmail.com>
2021-10-08 18:25:34 +0530

    added pusher backend

--- backend-src/server.js
+++ backend-src/server.js
@@ -5,58 +5,77 @@ const passport = require("passport")
 const cookieSession = require("cookie-session")
 const app = express()
 app.use(cors())
+app.use(express.json())
+// const User = require("./models/user.model")
 
-const User = require("./models/user.model")
+//pusher starts
+const Pusher = require("pusher")
+
+const pusher = new Pusher({
+  appId: "1279185",
+  key: "8c79b67adca098eeafac",
+  secret: "94b6dbba1d64c1e17845",
+  cluster: "ap2",
+  useTLS: true,
+})
+
+app.post("/api/messages", async (req, res) => {
+  await pusher.trigger("chat", "messages", {
+    username: req.body.username,
+    message: req.body.message,
+  })
+  res.json([])
+})
+
+// pusher ends
 
 // passport OAuth starts
-app.use(
-  cookieSession({
-    name: "linkedIn-demo",
-    keys: ["key1", "key2"],
-  })
-)
+// app.use(
+//   cookieSession({
+//     name: "linkedIn-demo",
+//     keys: ["key1", "key2"],
+//   })
+// )
 
-const isLoggedIn = (req, res, next) => {
-  if (req.user) {
-    next()
-  } else {
-    res.sendStatus(401)
-  }
-}
+// const isLoggedIn = (req, res, next) => {
+//   if (req.user) {
+//     next()
+//   } else {
+//     res.sendStatus(401)
+//   }
+// }
 
-app.use(express.json())
+// app.use(passport.initialize())
+// //app.use(passport.session())
 
-app.use(passport.initialize())
-//app.use(passport.session())
+// app.get("/failed", (req, res) => res.send("you failed to log in!"))
+// app.get("/", (req, res) => res.send("logged out"))
 
-app.get("/failed", (req, res) => res.send("you failed to log in!"))
-app.get("/", (req, res) => res.send("logged out"))
+// app.get(
+//   "/auth/google",
+//   passport.authenticate("google", { scope: ["profile", "email"] })
+// )
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-)
+// app.get(
+//   "/auth/google/callback",
+//   passport.authenticate("google", { failureRedirect: "/failed" }),
+//   async function (req, res) {
+//     // Successful authentication, redirect home.
 
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/failed" }),
-  async function (req, res) {
-    // Successful authentication, redirect home.
+//     const user = await User.find({ email: req.user._json.email }).lean().exec()
+//     if (user.length > 0) {
+//       res.redirect(`http://localhost:3000/profile/${req.user._json.email}`)
+//     } else {
+//       res.redirect(`http://localhost:3000/create-user/${req.user._json.email}`)
+//     }
+//   }
+// )
 
-    const user = await User.find({ email: req.user._json.email }).lean().exec()
-    if (user.length > 0) {
-      res.redirect(`http://localhost:3000/profile/${req.user._json.email}`)
-    } else {
-      res.redirect(`http://localhost:3000/create-user/${req.user._json.email}`)
-    }
-  }
-)
-
-app.get("/logout", (req, res) => {
-  req.session = null
-  req.logout()
-  res.redirect("/")
-})
+// app.get("/logout", (req, res) => {
+//   req.session = null
+//   req.logout()
+//   res.redirect("/")
+// })
 // passport OAuth ends
 
 const start = async () => {
