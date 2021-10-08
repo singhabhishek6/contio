775859b8f7bf54ff329e95ff12dab162ff756852
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-10-08 21:32:58 +0530

    Search page initiated

---
+++ src/Components/SearchResults/SearchResult.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { SearchStyle } from './SearchStyle'
+
+export const SearchResult = () => {
+    const x =[1,1,1,1,1]
+    return (
+       <SearchStyle>
+           {
+               x.map(el=>{
+                   return (
+                    <div class="a-box">
+  <div class="img-container">
+    <div class="img-inner">
+      <div class="inner-skew">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNjhBaCE6_IlqbjxTdth5Hm0mccqejntA2XA&usqp=CAU"/>
+      </div>
+    </div>
+  </div>
+  <div class="text-container">
+    <h3>A blue bird</h3>
+    <div>
+      This a demo experiment to skew image container. It looks good.
+  </div>
+</div>
+</div>
+                   )
+               })
+           }
+       </SearchStyle>
+    )
+}
