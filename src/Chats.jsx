01226a84e043a0d868df95221f57fbefff807af0
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-10-10 13:16:06 +0530

    review

--- src/Chats.jsx
+++ src/Chats.jsx
@@ -2,14 +2,13 @@ import React, { useRef, useState, useEffect } from "react"
 import { useHistory } from "react-router-dom"
 import { ChatEngine } from "react-chat-engine"
 import { auth } from "./Firebase"
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+
 import { useAuth } from "./Contexts/AuthContext"
 import axios from "axios"
 
 function Chats() {
   const history = useHistory()
-  const { user } = useAuth()
+  const { user,uid } = useAuth()
   console.log("user:", user)
   const [loading, setLoading] = useState(true)
   const handleLogout = async () => {
@@ -36,7 +35,7 @@ function Chats() {
         headers: {
           "project-id": "c401778f-0338-42f1-96ac-0c5119902933",
           "user-name": user.email,
-          "user-secret": user.uid,
+          "user-secret": uid,
         },
       })
       .then(() => {
@@ -46,7 +45,7 @@ function Chats() {
         let formdata = new FormData()
         formdata.append("email", user.email)
         formdata.append("username", user.email)
-        formdata.append("secret", user.uid)
+        formdata.append("secret", uid)
 
         getFile(user.photoURL).then((avatar) => {
           formdata.append("avatar", avatar, avatar.name)
@@ -70,17 +69,20 @@ function Chats() {
             })
         })
       })
-  }, [user, history])
-
-  if (!user || loading) return <img style={{position:"fixed",top:"50%",left:"40%"}} src="332.gif" alt="" />
+  }, [ ])
+console.log(user,"lkkj");
+  // if (!user || loading) return "Loading..."
 
   return (
     <div className="chats-page">
       <div className="nav-bar">
         <div className="logo-tab">Contio</div>
-        <div className="logout-tab" onClick={()=>{
-          history.goBack()
-        }}>
+        <div
+          className="logout-tab"
+          onClick={() => {
+            history.goBack()
+          }}
+        >
           Back
         </div>
       </div>
@@ -88,7 +90,7 @@ function Chats() {
         height="calc(100vh - 66px)"
         projectID="c401778f-0338-42f1-96ac-0c5119902933"
         userName={user.email}
-        userSecret={user.uid}
+        userSecret={uid}
       />
     </div>
   )
